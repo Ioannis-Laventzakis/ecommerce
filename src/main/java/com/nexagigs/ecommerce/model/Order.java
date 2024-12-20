aeeb136a45e3f387645cb3458e4b5d2c77e9697c
Ioannis <glaventzakis@gmail.com>
2024-12-20 23:25:04 +0200

    Initial commit

---
+++ src/main/java/com/nexagigs/ecommerce/model/Order.java
@@ -0,0 +1,4 @@
+package com.nexagigs.ecommerce.model;
+
+public class Order {
+}
--- src/main/java/com/nexagigs/ecommerce/model/Product.java
+++ src/main/java/com/nexagigs/ecommerce/model/Product.java
@@ -1,17 +1,48 @@
 package com.nexagigs.ecommerce.model;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(nullable = false,length = 100)
     private String name;
+
+    @Size(max = 255)
+    @Column(length = 255)
     private String description;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private double price;
+
+    @PositiveOrZero
+    @Column(nullable = false)
     private int quantity;
+
+    @Size(max = 255)
+    @Column(length = 255)
     private String image;
+
+    @Column(nullable = false)
     private boolean status;
 
     public Product() {
@@ -26,7 +57,7 @@ public class Product {
         this.image = image;
         this.status = status;
     }
-
+    // Getters and Setters
     public Long getId() {
         return id;
     }
@@ -82,4 +113,20 @@ public class Product {
     public void setStatus(boolean status) {
         this.status = status;
     }
+
+    // Override equals method
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+    // Override hashCode method
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
 }
+
