f65c0885f9dbd35d4b4f908b7871793619cca044
Ioannis <glaventzakis@gmail.com>
2024-12-20 22:55:21 +0200

    Initial commit

---
+++ src/main/java/com/nexagigs/ecommerce/model/Product.java
@@ -0,0 +1,85 @@
+package com.nexagigs.ecommerce.model;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    private String image;
+    private boolean status;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, double price, int quantity, String image, boolean status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.image = image;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
--- src/main/java/com/nexagigs/ecommerce/model/Role.java
+++ src/main/java/com/nexagigs/ecommerce/model/Role.java
@@ -1,8 +1,6 @@
 package com.nexagigs.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
 
 public enum Role {
     USER, ADMIN
