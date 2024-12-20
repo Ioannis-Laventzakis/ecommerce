799067786fbb2f717100b4479c81e4214d9aeff6
Ioannis <glaventzakis@gmail.com>
2024-12-20 21:30:31 +0200

    first commit

---
+++ src/test/java/com/nexagigs/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexagigs.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
