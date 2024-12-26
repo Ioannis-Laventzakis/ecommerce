7d5947da82f909760f6848b11eb954de5e4ab140
Ioannis <glaventzakis@gmail.com>
2024-12-26 11:01:06 +0200

    Initial commit

--- src/main/java/com/nexagigs/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nexagigs/ecommerce/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Handle UserNotFoundException
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
