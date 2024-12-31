6b2f8c867e116f31ec74a64d5fefaeaf8fbd8f7e
Ioannis <glaventzakis@gmail.com>
2024-12-31 12:54:15 +0200

    updateds

--- src/main/java/com/nexagigs/ecommerce/EcommerceApplication.java
+++ src/main/java/com/nexagigs/ecommerce/EcommerceApplication.java
@@ -3,6 +3,7 @@ package com.nexagigs.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// The @SpringBootApplication annotation is used to mark a configuration class that declares one or more @Bean methods and also triggers auto-configuration and component scanning.
 @SpringBootApplication
 public class EcommerceApplication {
 
